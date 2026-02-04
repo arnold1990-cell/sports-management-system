00e0f9b1ada2360bbd6d22e27bc991b66daad6c0
Arnold Tyvern Madamombe <arnoldmadaz@gmail.com>
2026-02-04 19:41:11 +0200

    Update fixtures and standings views

--- frontend/src/pages/StandingsPage.tsx
+++ frontend/src/pages/StandingsPage.tsx
@@ -115,6 +115,13 @@ const StandingsPage: React.FC = () => {
               <TableCell>{row.points}</TableCell>
             </TableRow>
           ))}
+          {!table.length && (
+            <TableRow>
+              <TableCell colSpan={9}>
+                <Alert severity="info">No standings available for the selected competition and season.</Alert>
+              </TableCell>
+            </TableRow>
+          )}
         </TableBody>
       </Table>
     </Box>
